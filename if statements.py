8f28053f57b073ec29e4195cce4c63d1d4be7968
thom0075 <68689661+thom0075@users.noreply.github.com>
2021-01-13 20:42:50 +0100

    Add files via upload

---
+++ if statements.py
@@ -0,0 +1,8 @@
+user_input_1=input("Inserisci il tuo nome: ")   
+
+if user_input_1=="Bob":
+    print("Welcome Bob!")
+elif user_input_1=="Root":
+    print("Welcome root user!")
+else:
+    print("Welcome %s "%(user_input_1))
